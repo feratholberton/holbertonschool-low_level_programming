83cddf73e12f27b4d6e794caf44c996ccb31269c
Fer <fernando.falcon@holbertonstudents.com>
2024-10-22 15:54:37 +0000

    space at the end of the first print

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	/* your code goes there */
 
 	last_digit = n % 10;
-	printf("Last digit of %d is %d\n", n, last_digit);
+	printf("Last digit of %d is %d ", n, last_digit);
 	if (last_digit > 5)
 	{
 		printf("and is grater than 5\n");
