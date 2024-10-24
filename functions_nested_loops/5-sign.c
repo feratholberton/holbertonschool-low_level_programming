592fddcbead6d0a8921f39757e4451e2654f2c1d
Fer <fernando.falcon@holbertonstudents.com>
2024-10-24 14:11:30 +0000

    Ifs with brakets

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,12 +9,18 @@
 int print_sign(int num)
 {
 	if (num > 0)
+	{
 		_putchar('+');
 		return (1);
+	}
 	else if (num == 0)
+	{
 		_putchar('0');
 		return (0);
+	}
 	else
+	{
 		_putchar('-');
 		return (-1);
+	}
 }
