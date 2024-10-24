1ebcf62c438132a77af72770e9b2988f0533a235
Fer <fernando.falcon@holbertonstudents.com>
2024-10-24 14:04:15 +0000

    Check que number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks if a number is less, greater or equals to zero
+ * @num: Value to check
+ * Return: 1 if value is positive, -1 if it is negative and 0 if it is zero
+ */
+
+int print_sign(int num)
+{
+	if (num > 0)
+		_putchar('+');
+		return (1);
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+	else
+		_putchar('-');
+		return (-1);
+}
