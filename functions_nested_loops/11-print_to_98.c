67366e8d12840c7f4405162e4cf95aa570f9ef4a
Fer <fernando.falcon@holbertonstudents.com>
2024-10-24 18:26:38 +0000

    Print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Write number until it gets to 98
+ * @n: number to start counting
+ * Return: times tables
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
