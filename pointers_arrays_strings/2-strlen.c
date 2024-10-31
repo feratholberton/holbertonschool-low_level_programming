9ba637ffe772a8bd086b68bc22ca0afba561e0ed
Fer <fernando.falcon@holbertonstudents.com>
2024-10-31 17:59:03 +0000

    Measure a string length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - get the length of a string
+ * @charString: Texto to measure
+ * Return: lenght
+ */
+
+int _strlen(char *charString)
+{
+	int length = 0;
+
+	while (charString[length] != '\0')
+	{
+		length++;
+	}
+
+	return length;
+}
