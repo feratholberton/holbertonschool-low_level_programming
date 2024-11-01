52c5af975b663db84270201765987d079ed48466
Fer <fernando.falcon@holbertonstudents.com>
2024-11-01 19:22:26 +0000

    Print even chars

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print even characters
+ *
+ * @string: to print 
+ *
+ * Return: void
+ */
+
+void puts2(char *string)
+{
+	int character = 0;
+
+	while (string[character] != '\0')
+	{
+		if (character % 2 == 0) 
+		{
+			_putchar(string[character]);
+		}
+
+		character++;
+	}
+	_putchar('\n');
+}
