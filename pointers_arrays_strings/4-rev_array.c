b313f4c5d1c3adfa5cc4a53856e538e8b4ed5e5b
Fer <fernando.falcon@holbertonstudents.com>
2024-11-04 15:51:03 +0000

    Betty comments and doc

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,7 +12,7 @@ void reverse_array(int *intsArr, int arrayItems)
 	int temp;
 	int i;
 
-	for (i = 0; i< arrayItems / 2; i++)
+	for (i = 0; i < arrayItems / 2; i++)
 	{
 		temp = intsArr[i];
 		intsArr[i] = intsArr[arrayItems - 1 - i];
