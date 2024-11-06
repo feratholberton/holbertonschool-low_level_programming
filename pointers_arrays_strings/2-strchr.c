d05a9afe82e1473c588c6a5f26f242385e8d5b83
Fer <fernando.falcon@holbertonstudents.com>
2024-11-06 15:18:56 +0000

    Betty comment style

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strchr - locates a character in a string
+ * @string: in which we look for
+ * @character: the character we look for
+ * Return: the string or NULL
+ */
+
 char *_strchr(char *string, char character)
 {
 	while (*string != '\0')
