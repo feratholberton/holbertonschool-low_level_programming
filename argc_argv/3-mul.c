1e5818c62b31151cc91d2a67071d54e8cd555053
Fer <fernando.falcon@holbertonstudents.com>
2024-11-13 13:12:53 +0000

    Multiply to arguments

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: var
+ * @argv: pointer
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	product = num1 * num2;
+
+	printf("%d\n", product);
+
+	return (0);
+}
