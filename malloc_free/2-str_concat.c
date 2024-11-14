08fb887c9c033ccd23f9d3cf4bb92dc69f8ddf81
Fer <fernando.falcon@holbertonstudents.com>
2024-11-14 19:02:50 +0000

    Concatenate 2 strings

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates 2 strings
+ * @stringOne: guess
+ * @stringTwo: guess
+ * Return: 2 strings concatenated
+ */
+
+char *str_concat(char *stringOne, char *stringTwo)
+{
+	unsigned int lengthOne, lengthTwo, i, ii;
+	char *result;
+
+	if (stringOne == NULL)
+	{
+		stringOne = "";
+	}
+	if (stringTwo == NULL)
+	{
+		stringTwo = "";
+	}
+	while (stringOne[lengthOne] != '\0')
+	{
+		lengthOne++;
+	}
+	while (stringTwo[lengthTwo] != '\0')
+	{
+		lengthTwo++;
+	}
+	result = malloc((lengthOne + lengthTwo + 1) * sizeof(char));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < lengthOne; i++)
+	{
+		result[i] = stringOne[i];
+	}
+	for (ii = 0; ii < lengthTwo; ii++)
+	{
+		result[lengthOne + ii] = stringTwo[ii];
+	}
+	result[lengthOne + lengthTwo] = '\0';
+	return (result);
+}
