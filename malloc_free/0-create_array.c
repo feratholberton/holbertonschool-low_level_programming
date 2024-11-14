f4d1cbde3bf00e3b0b8425c7c418d00e1b32ae12
Fer <fernando.falcon@holbertonstudents.com>
2024-11-14 15:42:42 +0000

    Array of chars

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+ * *create_array - guess
+ * @size: characters to allocate
+ * @character: char to start with
+ */
+
+char *create_array(unsigned int size, char character)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(size * sizeof(char));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = character;
+	}
+
+	return (array);
+}
