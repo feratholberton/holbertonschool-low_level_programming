b98a7c3875e026075cdb9d4459c01a41603dbf7f
Fer <fernando.falcon@holbertonstudents.com>
2024-11-21 21:00:42 +0000

    Betty style

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,6 +1,12 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - sums numbers
+ * @n: numbers
+ * Return: a sum
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
