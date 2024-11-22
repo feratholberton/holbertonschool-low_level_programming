f6caf93c1aa2c55f086de4a2a38ee44d56203739
Fer <fernando.falcon@holbertonstudents.com>
2024-11-22 13:31:34 +0000

    If removed

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,13 +1,11 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - sums numbers
- * @n: numbers
- * Return: a sum
+ * sum_them_all - sums all numbers given as arguments
+ * @n: numbers to sum
+ * Return: the sum of arguments or 0
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -32,4 +30,3 @@ int sum_them_all(const unsigned int n, ...)
 
 	return (sum);
 }
-#endif
