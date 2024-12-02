ae400e306cfe0f201aaf3653c7d743bd5e0a0cc2
Fer <fernando.falcon@holbertonstudents.com>
2024-12-02 15:50:36 +0000

    Remove braces

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,38 +9,32 @@
 
 char *str_concat(char *stringOne, char *stringTwo)
 {
-	unsigned int lengthOne, lengthTwo, i, ii;
+	unsigned int lengthOne = 0, lengthTwo = 0, i = 0, ii = 0;
 	char *result;
 
 	if (stringOne == NULL)
-	{
 		stringOne = "";
-	}
+
 	if (stringTwo == NULL)
-	{
 		stringTwo = "";
-	}
+
 	while (stringOne[lengthOne] != '\0')
-	{
 		lengthOne++;
-	}
+
 	while (stringTwo[lengthTwo] != '\0')
-	{
 		lengthTwo++;
-	}
+
 	result = malloc((lengthOne + lengthTwo + 1) * sizeof(char));
+
 	if (result == NULL)
-	{
 		return (NULL);
-	}
+
 	for (i = 0; i < lengthOne; i++)
-	{
 		result[i] = stringOne[i];
-	}
+
 	for (ii = 0; ii < lengthTwo; ii++)
-	{
 		result[lengthOne + ii] = stringTwo[ii];
-	}
+
 	result[lengthOne + lengthTwo] = '\0';
 	return (result);
 }
