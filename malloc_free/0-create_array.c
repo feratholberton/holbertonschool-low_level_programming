e9a783e191a93a9f26e628b34eb325f4a64b5e96
Fer <fernando.falcon@holbertonstudents.com>
2024-12-02 15:14:52 +0000

    Removed curly brackets

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,21 +13,15 @@ char *create_array(unsigned int size, char character)
 	unsigned int i;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
 
 	array = malloc(size * sizeof(char));
 
 	if (array == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < size; i++)
-	{
 		array[i] = character;
-	}
 
 	return (array);
 }
